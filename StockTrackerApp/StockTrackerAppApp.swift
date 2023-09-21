9b50c4f26215cb47c318cefa7b8c807accc114af
Matias Villar <matiase.villar@gmail.com>
2023-09-21 16:04:41 -0600

    Initial Commit

---
+++ StockTrackerApp/StockTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerAppApp.swift
+//  StockTrackerApp
+//
+//  Created by admin on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
